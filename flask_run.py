5f96f958edd1b25118ac0e95ff48de4f5876f296
zdzhang@ihep.ac.cn <zdzhang@ihep.ac.cn>
2023-02-12 03:04:47 +0800

    zzd

--- flask_run.py
+++ flask_run.py
@@ -4,18 +4,25 @@
 
 import os, sys
 from pathlib import Path
+import argparse
 
 here = Path(__file__).parent
 
 from HaiChatGPT.src.browser.app import run as run_app
 
-def run():
+def run(opt):
     # dir_ = f'{here}/HaiChatGPT/src/browser'
     # os.chdir(dir_)
-    run_app(host='0.0.0.0', port=5000, debug=True)
+    # run_app(host='0.0.0.0', port=5000, debug=True)
+    run_app(host=opt.host, port=opt.port, debug=opt.debug)
     # os.chdir(str(here))
 
 
 if __name__ == '__main__':
-    run()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--host', type=str, default='0.0.0.0')
+    parser.add_argument('--port', type=int, default=5000)
+    parser.add_argument('--debug', type=bool, default=False)
+    opt = parser.parse_args()
+    run(opt)
 
