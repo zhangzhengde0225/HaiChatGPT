b160a3a6aca4589251436de50d481ee28c0620b1
zdzhang@ihep.ac.cn <zdzhang@ihep.ac.cn>
2023-03-09 19:02:18 +0800

    zzd

--- HaiChatGPT/src/utils/check_network.py
+++ HaiChatGPT/src/utils/check_network.py
@@ -52,7 +52,7 @@ def try_request(share_value, api_key, proxies=None):
         "https://www.google.com",
         proxies=proxies,
         # headers=headers,
-        try_count=3,
+        timeout=5,
     )
     if reponse.status_code == 200:
         share_value.value = reponse.status_code
