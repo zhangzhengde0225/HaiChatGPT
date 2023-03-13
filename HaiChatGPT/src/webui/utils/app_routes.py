7546eda86de21c3f53cb3d57d4871ecd6ce648b7
zhangyiyu <zhangyiyu@ihep.ac.cn>
2023-03-13 17:35:51 +0800

    保存历史

--- HaiChatGPT/src/webui/utils/app_routes.py
+++ HaiChatGPT/src/webui/utils/app_routes.py
@@ -9,6 +9,8 @@ import traceback
 
 from ..app import app, webo
 from . import general
+from .user_manager import UserData, UserHistory, UserMessage, UserChat
+from ..app import db
 
 logger = dm.get_logger('app_routes')
 
@@ -30,13 +32,69 @@ def send_prompt():
         error_info = f'webo.query error: {e}. Traceback: {traceback.format_exc()}'
         logger.error(f'{error_info}')
         return jsonify({'success': False, 'message': error_info})
+    
     return jsonify({'success': True, 'message': prompt})
 
 @app.route('/stream')
 def stream(**kwargs):  # 即获取流式的last_answer
     user = general.get_user_from_session()
+    
+    # 保存 qa 到数据库
+    convo_id = 'default'
+    chat_id = convo_id
+
+    # TODO status 标识 history 代表不显示在会话中，defult表示显示在会话中
+    status = 'default'
+    chatbot = webo.get_bot_by_username(user, create_if_no_exist=False)
+    one_convo = chatbot.last_conversation
+    if one_convo is None:
+        pass
+    else:
+        role, convo_id, query, text = one_convo
+        print("send_prompt: one_convo")
+        print(one_convo)
+        
+        # 搜索用户
+        user = UserData.query.filter_by(name=user).first()
+        if not user:
+            # TODO 每次会话创建有一个唯一ID的临时用户
+            user = UserData(name="quest")
+            db.session.add(user)
+            db.session.commit()
+        
+        # TODO 测试 保存 chat
+        chat = UserChat.query.filter_by(
+                user_id=user.id, chat_id=chat_id).first()
+        if not chat:
+            chat = UserChat(user_id=user.id, chat_id=chat_id)
+            db.session.add(chat)
+            db.session.commit()
+
+        # TODO 测试 保存 Message
+        message = UserMessage(
+            chat_id=chat.id, query=query, text=text, status=status)
+        db.session.add(message)
+        db.session.commit()
+        '''
+        读取Message示例：
+        messages = Message.query.filter_by(chat_id=chat.id).all()
+        result = []
+        for message in messages:
+            result.append({'sender': message.sender, 'message': message.message})
+        return jsonify(result)
+        '''
+
+        # 保存历史记录
+        user_history = UserHistory(
+            role=role, convo_id=convo_id, query=query, text=text, status=status, user_id=user.id)
+        db.session.add(user_history)
+        db.session.commit()
+    
+    # 必须清楚缓存，不然会保存两遍，原因可能是 route('/stream') 会在显示聊天时调用两次
+    chatbot.last_conversation = None
+
     logger.debug(f'收到stream请求: Method: {request.method}. User: {user}. Kwargs: {kwargs}')
-    stream_buffer = webo.get_stream_buffer(user)
+    stream_buffer = webo.get_stream_buffer(user) 
     if stream_buffer is None:
         ret = f"data: <|im_end|>\n\n"
     else:
@@ -56,6 +114,10 @@ def qa_pairs():
         return Response(f"data: {data}\n\n", mimetype="text/event-stream")
     # logger.debug(f'收到qa_pairs请求: Method: {request.method}. User: {user}. chatbot: {chatbot}')
     # logger.debug(f'chatbot.show_history: {chatbot.show_history}')
+    
+    
+
+    # TODO 处理history
     history = webo.get_history(user)
     if history == [] or history is None:
         data = '<|im_end|>'
@@ -63,6 +125,6 @@ def qa_pairs():
         data = history
         chatbot.show_history = False
 
-    ret = f"data: {data}\n\n"
-    logger.debug(f'返回qa_pairs响应: {ret}')
+    ret = f"data: {data}\n\n"    
+    logger.debug(f'返回qa_pairs响应: {ret}')  
     return Response(ret, mimetype="text/event-stream")
