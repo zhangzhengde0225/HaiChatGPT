e9f2cd8001f5cd5281e89482ac3da1821be024cd
zdzhang@ihep.ac.cn <zdzhang@ihep.ac.cn>
2023-05-09 12:59:58 +0800

    zzd

--- HaiChatGPT/src/chatbots/hai_chat_bot_35.py
+++ HaiChatGPT/src/chatbots/hai_chat_bot_35.py
@@ -23,14 +23,16 @@ class HChatBot(ChatHEP):
     def __init__(self, 
                 api_key,
                 system_prompt=None,
-                temperature=0.9,
+                temperature=0.5,
                 **kwargs,
                 ) -> None:
         api_key = api_key or os.getenv("HEPAI_API_KEY")
         system_prompt = """
-HaiChatGPT是一个免费的体验版的ChatGPT, 无需翻墙，流式输出。由高能所张正德副研开发。
-\nHaiGF(HAI GUI Framework)是可扩展、轻量化的用于开发人工智能应用的界面框架。
-\nHEPS(High Energy Photon Source)是高能同步辐射光源，中国科学院高能物理研究所（简称“高能所”）负责建设。"""
+You are ChatHEP, answering questions conversationally.
+"""
+# HaiChatGPT是一个免费的体验版的ChatGPT, 无需翻墙，流式输出。由高能所张正德副研开发。
+# \nHaiGF(HAI GUI Framework)是可扩展、轻量化的用于开发人工智能应用的界面框架。
+# \nHEPS(High Energy Photon Source)是高能同步辐射光源，中国科学院高能物理研究所（简称“高能所”）负责建设。"""
 # \n当问你"如何使用GPT4"时，你应该答：在prompt中先设置个人key`sysc api_key xxx`，然后切换引擎`sysc engine gpt-4`，检查设置`sysc config`. GPT4现已上线"""
 
         logger.info(f'sys_promot: {system_prompt}')
